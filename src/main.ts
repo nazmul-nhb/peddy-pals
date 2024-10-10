cba9bd7b00d0cfa3cace7de7781d0dc238fc336f
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-10-10 12:52:17 +0600

    added footer, subscribe function; fixed some ui issues

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import "./style.css";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { displayAllCategories } from "./modules/displayAllCategories";
 import { loadAllPets } from "./modules/loadAllPets";
+import { getElementById } from "./utilities/utilities";
+
+const subscribeButton = getElementById("subscribe");
 
 loadAllPets();
 
 displayAllCategories();
+
+// Subscribe Button
+subscribeButton?.addEventListener("click", (e: MouseEvent) => {
+	e.preventDefault();
+
+	const emailInput = getElementById("email") as HTMLInputElement;
+	const email = emailInput.value.trim();
+
+	if (!email) {
+		return toastr.error("Please, Provide Your Email!");
+	}
+
+	toastr.success("Subscribed to Newsletter!");
+	emailInput.value = "";
+});
